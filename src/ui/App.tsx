a6229c58bcb9e427c0f95e27a727fea0e815697b
Emu Rom Man <romulus.dmg@gmail.com>
2026-02-16 01:35:25 -0500

    Configured shadcn and theme

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,10 +1,7 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   useEffect(() => {
     const list: DownloadItem[] = [
       { title: "007 James Bond (US)", url: "myrient.us" },
@@ -14,23 +11,9 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex min-h-svh w-full flex-col items-center justify-center">
+        <Button variant="default">Click me</Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
